3df77d412406c7b2dcc0ce27c74fe7072a1ad670
joris compernol <joris.compernol@euri.com>
2016-10-18 15:46:09 +0200

    moved styles to common

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 import '../node_modules/bootstrap/scss/bootstrap.scss';
-import './views/common/styles/styles.scss';
+import './common/styles/styles.scss';
 
 @Component({
     selector: 'my-app',
