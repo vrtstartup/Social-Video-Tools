70c32008c32a888d257f624080f046ac16502eff
Matthias De Vriendt <devriendt.matthias@gmail.com>
2016-10-19 16:32:32 +0200

    project class

---
+++ models/project.model.ts
@@ -0,0 +1,45 @@
+export class Project { 
+    projectId: string; 
+    userId: string;
+    name: string;
+    projectPath: string;
+
+    clip: any;
+
+    constructor (project?: any) {
+      this.projectId = (typeof project === 'undefined') ? "null" : project.projectId;
+      this.userId = (typeof project === 'undefined') ? "null" : project.userId;
+      this.name = (typeof project === 'undefined') ? "null" : project.name;
+      this.projectPath = (typeof project === 'undefined') ? "null" : project.projectPath;
+
+      this.clip = {
+        "lowResUrl": (typeof project === 'undefined') ? "null" : project.clip.lowResUrl,
+        "highResUrl": (typeof project === 'undefined') ? "null" : project.clip.highResUrl,
+        "fileName": (typeof project === 'undefined') ? "null" : project.clip.fileName
+      }
+    }
+
+    test() {
+      console.log(this.projectId);
+    }
+
+    data() {
+      /*
+      * serialize properties and return a json object 
+      * ready for storage in Firebase
+      */
+
+      return {
+        "projectId": this.projectId,
+        "userId": this.userId,
+        "name": this.name,
+        "projectPath": this.projectPath,
+        "clip": {
+          "lowResUrl": this.clip.lowResUrl,
+          "highResUrl": this.clip.highResUrl,
+          "fileName": this.clip.fileName,
+        }
+      };
+    }
+}
+
