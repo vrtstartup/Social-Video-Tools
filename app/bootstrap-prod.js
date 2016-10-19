aca12cbb259b2013c9e2d9aa0a12527444cbf883
Matthias De Vriendt <devriendt.matthias@gmail.com>
2016-10-19 12:16:31 +0200

    ts-node garbage

--- app/bootstrap-prod.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_1 = require('@angular/platform-browser');
-var app_module_ngfactory_1 = require('../aot/app/app.module.ngfactory');
-platform_browser_1.platformBrowser().bootstrapModuleFactory(app_module_ngfactory_1.AppModuleNgFactory);
-//# sourceMappingURL=bootstrap-prod.js.map
