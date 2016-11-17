6a0672a09bcee2f2f3985b90755919fa99962787
joris compernol <joris.compernol@euri.com>
2016-11-17 14:19:48 +0100

    name change in template.models

---
+++ app/views/public/subtitles/models/annotationTemplate.model.ts
@@ -0,0 +1,7 @@
+export default {
+    type: '',
+    start: '0',
+    end: '3',
+    data: '',
+    'render-status': ''
+}
--- app/views/public/subtitles/testModels/annotationTemplate.model.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    type: 'overlay',
-    start: '0.20',
-    end: '1.20',
-    data: '',
-    "render-status": 'ready'
-}
