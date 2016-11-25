ded500d920fb04507a93eae0158120aa5f70ab11
joris compernol <joris.compernol@euri.com>
2016-11-25 11:25:59 +0100

    remove project-fn to model (wip)

--- app/views/public/subtitles/models/project.model.ts
+++ app/views/public/subtitles/models/project.model.ts
@@ -1,3 +1,6 @@
+import { ListPipe } from '../../../../common/pipes/list.pipe';
+import { SortByPropPipe } from '../../../../common/pipes/sortByProp.pipe';
+
 export class Project {
 
     public data;
@@ -7,15 +10,64 @@ export class Project {
         delete project['$exists'];
         this.data = project;
     }
-
+    
     getAnnotations() {
         return this.data.annotations;
     }
-    
-    setAnnotations() {
-        return this.data.annotations;
-    }
-    updateAnnotations(){
 
+    addAnnotation(template){
+        // create annotation object if none
+        if (!this.data['annotations']) {
+            this.data['annotations'] = {};
+        }
+
+        let strtTm = 0;
+        let spanTm = 4;
+
+        // if min one annotation
+        if (Object.keys(this.data['annotations']).length > 0) {
+            // object to array => sort => get one with highest end-value
+            let annoArray = new ListPipe().transform(this.data['annotations']);
+            let annoSorted = new SortByPropPipe().transform(annoArray, 'end');
+            let annoLastEndtime = annoSorted[annoSorted.length -1 ]['end'] 
+            
+            strtTm = annoLastEndtime;
+            const leftTm = this.data['clip']['movieLength'] - strtTm;
+
+            if (leftTm <= spanTm) {
+                strtTm = this.data['clip']['movieLength'] - spanTm;
+            }
+        }
+
+        let endTm = strtTm + spanTm;
+        let newId = this.makeid();
+        let newAnno = {
+            key: newId,
+            start: strtTm,
+            end: endTm,
+            data: template,
+        };
+        
+        this.data['annotations'][newId] = newAnno;
+
+        return newAnno
     }
+
+    setSelectedAnno(key) {
+        return this.data['annotations'][`${key}`];
+    }
+
+    updateSelectedAnno(key, obj){
+        // update project | if key doesn't exists, its created this way
+        this.data['annotations'][`${key}`] = obj;
+    }
+
+    private getLastObject(obj) {
+        return obj[Object.keys(obj)[Object.keys(obj).length - 1]];
+    }
+
+    private makeid() {
+        return `-ANNO${Math.random().toString(22).substr(2, 15).toUpperCase()}`;
+    }
+    
 }
