a0c51caa1108c82a54791088b3514f458d549e02
joris compernol <joris.compernol@euri.com>
2016-12-14 14:30:49 +0100

    projects list filter

--- app/common/services/project.service.ts
+++ app/common/services/project.service.ts
@@ -1,21 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Observable, Subject } from 'rxjs/Rx';
 import { Project } from '../models/project.model';
 import { AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Injectable()
-export class ProjectService{
+export class ProjectService {
     /*
     * map the AngularFire /projects stream to a new stream, which wraps the raw data
     * in Project()-class objects.
     */
+    public usersQuerySubject: Subject<any>;
+    public userQuerySubject: Subject<any>;
+
     public projects$: Observable<any>;
+    public projectsByUser$: Observable<any>;
 
     constructor(private af:AngularFire) {
-      this.projects$ = af.database.list('/projects').map(projectsData => {
-        const arrProjects: Array<Project> = [];
+
+      this.usersQuerySubject = new Subject();
+      this.userQuerySubject = new Subject();
+
+      this.projects$ = af.database.list('/projects', {
+        query: { 
+          limitToLast: this.usersQuerySubject.map(d => {return d.last}),
+        }
+      }).map(projectsData => {
+        let arrProjects: Array<Project> = [];
         projectsData.forEach(projectData => arrProjects.push(new Project(projectData)));
+
         return arrProjects;
       });
+      
+      this.projectsByUser$ = af.database.list(`/projects`, { 
+        query: { 
+          limitToLast: this.userQuerySubject.map(d => {return d.last}),
+          orderByChild: 'createdBy',
+          equalTo: this.userQuerySubject.map(d => {return d.email})
+        }
+      }).map(projectsData => {
+        let arrProjects: Array<Project> = [];
+        projectsData.forEach(projectData => arrProjects.push(new Project(projectData)));
+
+        return arrProjects;
+      })
+
+    }
+
+    setUsersQuerySubject(usersQuery){
+      this.usersQuerySubject.next(usersQuery);
+    }
+    
+    setUserQuerySubject(userQuery){
+      this.userQuerySubject.next(userQuery);
     }
 }
--- app/views/common/project-list.component.ts
+++ app/views/common/project-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProjectService } from '../../common/services/project.service';
 import { Project } from '../../common/models/project.model';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'project-list',
@@ -10,21 +12,50 @@ import { Project } from '../../common/models/project.model';
 
 export class ProjectListComponent implements OnInit {
   @Input() role: number;
+
+  public projectsByUser: any;
   private projects: Array<Project>;
+  public userEmail = '';
 
   constructor(
+      private af:AngularFire,
       private projectService: ProjectService, 
       private router: Router) {
+      this.af = af;
+      this.userEmail = 'joris.compernol@vrt.be';
   }
 
   ngOnInit(){
-      this.projectService.projects$.subscribe( projects => {
-        this.projects = projects;
+      // query by userId
+      this.projectService.projectsByUser$.subscribe( projectsbyuser => {
+        this.projects = projectsbyuser;
       })
+      const userQuery = { 
+        email: this.userEmail, 
+        last: 10,
+      };
+      this.projectService.setUserQuerySubject(userQuery);
   }
 
   open(projectId: string){ 
     //this.selectionUpdated.emit(projectId);
     this.router.navigateByUrl(`/projects/${projectId}`);
   }
+
+  setUserByEmail(formdata) {
+      // TODO check if email exists and inform user
+      if(formdata.email === '') {
+        // query all project if no email adress provided
+        this.projectService.projects$.subscribe( projects => { this.projects = projects;})
+        const usersQuery = {last: 30};
+        this.projectService.setUsersQuerySubject(usersQuery);
+        return
+      }
+      const userQuery = { 
+        email: formdata.email, 
+        last: 10,
+      };
+      this.projectService.setUserQuerySubject(userQuery);
+  }
+
 }
