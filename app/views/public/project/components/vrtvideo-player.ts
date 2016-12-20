e5e81a3b97895c7081649e958d1fc66c76c7be8a
joris compernol <joris.compernol@euri.com>
2016-12-20 20:18:58 +0100

    vrt-player  fixes

--- app/views/public/project/components/vrtvideo-player.ts
+++ app/views/public/project/components/vrtvideo-player.ts
@@ -29,23 +29,21 @@ export class VrtVideoPlayer implements OnChanges, OnInit {
         this.apiLoaded = false;
     }
 
+    ngOnInit(){   
+    }
+
     onPlayerReady(api:VgAPI) {
         this.api = api;
         this.apiLoaded = true;
-        this.setSeekTime();
     }
 
-    ngOnInit(){
+    ngOnChanges(changes: SimpleChanges) { 
         
-    }
-
-    ngOnChanges(changes: SimpleChanges) {
-
-        if(this.selectedAnnotationKey && this.annotations){
+        //console.log('changes', changes);
+        if(this.apiLoaded && this.selectedAnnotationKey && this.annotations){
             this.selectedAnnotation = this.annotations[this.selectedAnnotationKey];
+            this.setSeekTime();
         }
-
-        this.setSeekTime();
 
         if(changes.hasOwnProperty('clip')) { // input 'clip' changed
             const prev = changes['clip']['previousValue'];
@@ -55,44 +53,32 @@ export class VrtVideoPlayer implements OnChanges, OnInit {
                 this.sources = [this.clip];
             }
         }
-
-        console.log(changes);
-        if(changes.hasOwnProperty('selectedAnnotation') && changes['selectedAnnotation']['currentValue'] && this.apiLoaded) { // input 'selectedAnnotation' changed && VG api loaded?
-            const prev = changes['selectedAnnotation']['previousValue'];
-            const curr = changes['selectedAnnotation']['currentValue'];
-
-            if(prev != null && curr !=null && prev['key'] != curr['key']) this.setSeekTime();
-        } 
-
     }
 
     setSeekTime(){
+
         // position the seek time according to the selected annotation
         //  subscribe to seek time to reset seek position whenever it goes out of the bounds defined by the scrub handles 
-        if(this.selectedAnnotation && this.apiLoaded) {
-            let seekTime = Number(this.selectedAnnotation['start']);
-            this.api.seekTime(seekTime);
+        let seekTime = parseFloat(this.selectedAnnotation['start']);
+        this.api.seekTime(seekTime);
+        this.api.play();
 
-            // loop between scrub handles
-            this.api.getDefaultMedia().subscriptions.timeUpdate.subscribe(() => {
-                this.currentTime = Number(this.api.currentTime);
-
+        // loop between scrub handles
+        this.api.getDefaultMedia().subscriptions.timeUpdate.subscribe(() => {
+            this.currentTime = Number(this.api.currentTime);
+            
+            if (this.api.currentTime >= parseFloat(this.selectedAnnotation.end)) {    
+                this.api.seekTime(parseFloat(this.selectedAnnotation.start));
                 this.api.play();
-
-                if (this.api.currentTime >= parseFloat(this.selectedAnnotation.end)) {
-                    this.api.seekTime(parseFloat(this.selectedAnnotation.start));
-                    this.api.play();
-                }
-            });
-        }
-
+            }
+        });
     }
 
     parseHtml(annotation) {
-        // value in template e.g '<div>%keyValue%</div>' will be replaced by 
-        // the textvalue of corresponding keyValue
 
-        // JSON.stringify(this.selectedAnnotation.data.text) !== JSON.stringify(annotation.data.text)
+         // value in template e.g '<div>%keyValue%</div>' will be replaced by 
+         //  the textvalue of corresponding keyValue
+
         if (annotation.data.text) {
             
             let parsedHtml = annotation.data.templateHtml;
@@ -109,13 +95,9 @@ export class VrtVideoPlayer implements OnChanges, OnInit {
             $(`#${annotation.key}`).replaceWith(templateHtml);
         }
         
-        // for text: replace html string with text values
+        // TODO for layers: replace html string with text values
         if(annotation.data.layer){
 
         }
-    }
-
-    isEmptyObject(obj){ 
-        return (typeof obj === 'object') ? (Object.keys(obj).length === 0) : true;
     }
 }
